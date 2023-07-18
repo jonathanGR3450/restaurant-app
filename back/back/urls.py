cb76730d1f3746be6dd4428e4315e0c0803f7a30
jonathan garzon <jonatangarzon95@gmail.com>
2023-07-18 16:51:10 -0500

    feat: use swager documentation

--- back/back/urls.py
+++ back/back/urls.py
@@ -14,8 +14,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="iCard - ApiDoc",
+        default_version='v1',
+        description="api icard documentation",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="jonatangarzon95@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+   public=True,
+#    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
