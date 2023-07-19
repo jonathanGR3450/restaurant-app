c9d1a7916aa2573981239c1e2c2be5047382b51d
jonathanGR3450 <52362305+jonathanGR3450@users.noreply.github.com>
2023-07-19 15:07:32 -0500

    feature: routes structure

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import { Button } from "semantic-ui-react";
-import "./App.scss"
+import React from "react";
+import { Navigation } from "./routes";
 
 export default function App() {
   return (
     <div>
       <h1>Hola mundo</h1>
-      <Button primary>Click me</Button>
+      {/* <Navigation></Navigation> */}
     </div>
-  )
+  );
 }
---
+++ front/src/routes/Navigation.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Routes } from "react-router-dom";
+
+export function Navigation() {
+  return (
+    <Routes>
+      <h1>Navigation...</h1>
+    </Routes>
+  );
+}
